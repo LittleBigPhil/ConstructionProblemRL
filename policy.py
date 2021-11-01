b523a7c0b905408978be133fb84aa2f62ff82aaf
Phillip Roshon <philliproshon@gmail.com>
2021-11-01 07:48:52 -0400

    Implemented lazy adding of instantiations to the soft queue.

--- policy.py
+++ policy.py
@@ -8,29 +8,38 @@ class SoftQueuePolicy:
     """A policy for picking instantiations based on a soft priority queue."""
     def __init__(self, policy = UniformWeighter()):
         config = Configuration.load()
-        self.queue = SoftQueue(sensitivity=config.softMaxSensitivity, offset=config.softMaxOffset)
+        self.softQueue = SoftQueue(sensitivity=config.softMaxSensitivity, offset=config.softMaxOffset)
+        self.addQueue = []
         self.policy = policy
     def clear(self):
-        """Empties the queue."""
-        self.queue = SoftQueue()
+        """Empties the soft queue."""
+        self.softQueue = SoftQueue()
+        self.addQueue.clear()
     def add(self, action, features):
-        """Adds a new instantiation to the queue."""
-        self.queue.add(action, self.policy(features))
+        """Adds a new instantiation to the add queue."""
+        self.addQueue.append((action, features))
+    def __resolveAddQueue(self):
+        """Adds all instantiation from the add queue to the soft queue."""
+        for action, features in self.addQueue:
+            self.softQueue.add(action, self.policy(features))
+        self.addQueue.clear()
     def resolve(self):
-        """Removes an element from the queue according to the probability."""
-        return self.queue.pop()
+        """Removes an element from the soft queue according to the probability."""
+        self.__resolveAddQueue()
+        return self.softQueue.pop()
     def sampleActionSpace(self, toSample: int):
-        toSample = max(toSample, len(self.queue))
+        self.__resolveAddQueue()
+        toSample = max(toSample, len(self.softQueue))
 
         sampled = []
         while toSample > 0:
-            index = self.queue.sample()
+            index = self.softQueue.sample()
             if not index in sampled:
                 sampled.append(index)
                 toSample -= 1
             # Should probably add a special sampling procedure that can't do repeats.
 
-        return [self.queue[i].object for i in sampled]
+        return [self.softQueue[i].object for i in sampled]
 
 
 
